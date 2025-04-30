b622acc46c991845b263619ed050d2b7907c41d6
Rendi <artemrepin00@gmail.com>
2025-04-30 13:30:20 +0300

    Win screen ui

--- Assets/_Game/Scripts/View/UI/Gameplay/WinScreen/ScreenWin/ScreenWinView.cs
+++ Assets/_Game/Scripts/View/UI/Gameplay/WinScreen/ScreenWin/ScreenWinView.cs
@@ -6,14 +6,26 @@ public class ScreenWinView : WindowView<ScreenWinViewModel>
 {
 
     [SerializeField] private Button _buttonContinue;
+    [SerializeField] private Button _buttonSkip;
 
     private void OnEnable()
     {
         _buttonContinue.onClick.AddListener(OnContinueButtonClicked);
+        _buttonSkip.onClick.AddListener(OnSkipButtonClicked);
+    }
 
+    private void OnDisable()
+    {
+        _buttonContinue.onClick.RemoveAllListeners();
+        _buttonSkip.onClick.RemoveAllListeners();
     }
 
     private void OnContinueButtonClicked()
+    {
+        ViewModel.ContinueLevel();
+    }
+
+    private void OnSkipButtonClicked()
     {
         ViewModel.ContinueLevel();
     }
